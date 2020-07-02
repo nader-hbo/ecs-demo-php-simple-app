b8e8a3102bcaaf4e63cee6387bf49d1a5c191322
Nader Nashaat <nader.nashaat@hbo.com>
2020-07-02 11:29:42 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -19,7 +19,8 @@
                 <h1>PHP new feature</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+		<p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+		<p>this is version 001 </p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
