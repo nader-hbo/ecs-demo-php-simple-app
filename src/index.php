6247384ce1dcd605115f612c3597dd35c1394474
Nader Nashaat <nader.nashaat@hbo.com>
2020-07-02 17:09:09 +0200

    Version2

--- src/index.php
+++ src/index.php
@@ -20,7 +20,7 @@
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
 		<p>The container is running PHP version <?php echo phpversion(); ?>.</p>
-		<p>this is version 001 </p>
+		<p>this is version 002 </p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
